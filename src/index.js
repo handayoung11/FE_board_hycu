849307152572ea707b6632a3eb7ea276d6f4c15e
JinYeongKim <handayoung11@gmail.com>
2023-09-14 16:50:01 +0900

    react router 설정 추가

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import FeedView from './page/FeedView';
 import FeedDetail from './page/FeedDetail';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FeedView />,
+  },
+  {
+    path: "/page/:page",
+    element: <FeedView />,
+  },
+  {
+    path: "/post/:postId",
+    element: <FeedDetail />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <FeedDetail />
+        <RouterProvider router={router} />
   </React.StrictMode>
 );
